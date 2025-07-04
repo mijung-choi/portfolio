f67901c5a9f113ecb113b9bb7181e92ccbdeaf36
choimijung <chmj56@gmail.com>
2025-07-04 18:59:14 +0900

    사진 크기 맞추기

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ console.log("포트폴리오 페이지에 오신 걸 환영합니다!");
 const customCursor = document.querySelector(".custom-cursor");
 
 document.addEventListener("mousemove", (e) => {
-  customCursor.style.left = `${e.pageX}px`;
-  customCursor.style.top = `${e.pageY}px`;
+  customCursor.style.left = `${e.clientX}px`;
+  customCursor.style.top = `${e.clientY}px`;
 });
 
 function toggleProjects(id) {
